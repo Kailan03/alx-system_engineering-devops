344a73ca0b5c52cc53b906374c7247e0f3475642
Rahmat <rahmatbola3@gmail.com>
2024-01-30 18:28:18 -0800

    regex task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # Check if the command-line argument is provided
 # shellcheck disable=SC2009
-
+#
 if ARGV.length != 1
   puts "Usage: #{$PROGRAM_NAME} <input_string>"
   exit 1
@@ -11,7 +11,7 @@ end
 input_string = ARGV[0]
 
 # Use the Oniguruma regular expression to match "School"
-match_result = input_string.scan(/School/)
+match_result = input_string.scan(/Scho.l/i)  # 'i' makes the match case-insensitive
 
 # Check if there is a match and print the result
 if match_result.length > 0
@@ -19,4 +19,3 @@ if match_result.length > 0
 else
   puts ""
 end
-
