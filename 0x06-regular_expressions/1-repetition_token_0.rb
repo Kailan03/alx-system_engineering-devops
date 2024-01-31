803ced3a8a9e1700221342d07d8c95f3412f8c5a
Rahmat <rahmatbola3@gmail.com>
2024-01-30 19:06:54 -0800

    m

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -14,8 +14,8 @@ input_string = ARGV[0]
 match_result = input_string.match(/hb[t]+n/i)
 
 # Check if there is a match and print the result
-if match_result.length > 0
-  puts "#{match_result.join}"
+if match_result && match_result.length > 0
+  puts "#{match_result[0]}"
 else
   puts ""
 end
