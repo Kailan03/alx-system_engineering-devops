a3073cf9d3801be7403631f2599c013a01f2776e
Rahmat <rahmatbola3@gmail.com>
2024-05-07 17:45:10 -0700

    RedirectError

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyBot/1.0"}  # to avoid Too Many Requests error
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
